ddc902d4484df995a91dc9ab188aab71b7882714
Chukwudi Steven Ubah <chuksgpfr@gmail.com>
2021-03-07 20:40:40 +0100

    Create select.sql

---
+++ select.sql
@@ -0,0 +1,5 @@
+//For this challenge you will be performing a simple query on an SQL database.
+
+/* write your SQL query below */
+
+SELECT * FROM maintable_P3TN2 WHERE LastName = "Smith" OR FirstName ="Robert" ORDER BY Age ASC;
